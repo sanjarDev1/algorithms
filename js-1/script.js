c180a7a7c2155fcd2bc2a7c1987193ea0e213558
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-20 19:37:27 +0500

    1-task

---
+++ js-1/script.js
@@ -0,0 +1,45 @@
+ // 1-task
+
+var a = 5;
+
+console.log(a * 4);
+
+// 2-task
+
+var a = 10;
+
+console.log(10 ** 2);
+
+// 3-task
+
+var a = 12;
+var b = 8;
+
+console.log(a * b);
+console.log(2 * (a + b));
+
+// 4-task
+
+var d = 20;
+const pi = 3.14;
+
+console.log(pi * d);
+
+// 5-task
+
+var a = 5;
+
+console.log(a ** 3);
+console.log(6 * (a * a));
+
+// 6-task
+
+var a, b, c;
+a = 8;
+b = 6;
+c = 14;
+
+console.log(a * b * c);
+console.log(2 * (a * b + b * c + a * c));
+
+
