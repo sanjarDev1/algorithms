787a9a2018cdc952905cf4f4aef2137e7596d55c
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-21 00:26:33 +0500

    5-10 task

--- js-1/script.js
+++ js-1/script.js
@@ -42,4 +42,38 @@ c = 14;
 console.log(a * b * c);
 console.log(2 * (a * b + b * c + a * c));
 
+// 7-task
+
+var r = 12;
+const pi = 3.14;
+console.log(2 * pi*r);
+console.log(pi * r ** 2);
+
+// 8-task
+
+var a = 55;
+var b = 45;
+
+console.log((a + b) / 2);
+
+// 9-task
+var a = 10;
+var b = 20;
+
+console.log((a * b) **(1/2));
+
+
+// 10-task
+
+var a = 8;
+var b = 16;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a ** 2 + " " + b ** 2);
+
+// 11-task
+
+
+
 
